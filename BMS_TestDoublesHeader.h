12b1487cc9c678643f6840853f86a88edc59a2f5
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-05-23 19:16:51 +0530

    Add files via upload

---
+++ BMS_TestDoublesHeader.h
@@ -0,0 +1,10 @@
+
+extern char* Test_ConsoleOutput;
+extern int printf_Func_CallCount;
+
+
+extern int ResetTestInterface();
+extern int Rand();
+extern int printf(char* ConsoleContent);
+
+
---
+++ BSM_TestDoubles.c
@@ -0,0 +1,44 @@
+#include "SignalProvider.h"
+#include <stddef.h>
+
+#define UNIT_TEST
+
+char* Test_ConsoleOutput=NULL;
+int printf_Func_CallCount=0;
+
+
+
+int ResetTestInterface()
+{
+
+Test_ConsoleOutput=NULL;
+printf_Func_CallCount=0;
+	
+}
+
+int Rand()
+{
+	static int Itr=0;
+	int RandomValue=0;
+	for(;Itr<15;i++)
+	{
+		if(!(Itr%2))
+		{
+			RandomValue=Itr;
+		}
+		else
+		{
+			RandomValue=32767-Itr;
+		}
+	}
+	return RandomValue;
+}
+
+
+int printf(char* ConsoleOutput)
+{
+	Test_ConsoleOutput=ConsoleOutput;
+	printf_Func_CallCount++;
+	return 1;
+}
+

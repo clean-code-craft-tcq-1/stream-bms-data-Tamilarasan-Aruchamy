652c9b2572f350979eb380f3599f8080389dbd48
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-05-23 20:41:46 +0530

    Update BMS_TestDoublesHeader.h

--- BMS_TestDoublesHeader.h
+++ BMS_TestDoublesHeader.h
@@ -7,6 +7,6 @@ extern int printf_Func_CallCount;
 
 extern int ResetTestInterface();
 extern int rand();
-extern int printf(char* ConsoleContent);
+int printf(char* ConsoleOutput,char* ConsoleOutputValue)
 
 

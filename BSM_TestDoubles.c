c4a0281ee46e69c3388bf0e4193ebd28f5542390
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-05-24 21:48:28 +0530

    Update BSM_TestDoubles.c

--- BSM_TestDoubles.c
+++ BSM_TestDoubles.c
@@ -32,16 +32,19 @@ int rand()
 	int Rand_Temperature[15]={0,1,2,3,4,5,6,7,RAND_MAX-7,RAND_MAX-6,RAND_MAX-5,RAND_MAX-4,RAND_MAX-3,RAND_MAX-2,RAND_MAX-1};
 	int Rand_SOC[15]={0,1,2,3,4,5,6,7,RAND_MAX-7,RAND_MAX-6,RAND_MAX-5,RAND_MAX-4,RAND_MAX-3,RAND_MAX-2,RAND_MAX-1};
 	
+	static int Itr=0;
 	int RandomValue=0;
-	for(static int Itr=0;Itr<30;Itr++)
+	if(Itr<30)
 	{
 		if(!(Itr%2))
 		{
 			RandomValue=Rand_Temperature[14];
+			Itr++;
 		}
 		else
 		{
 			RandomValue=Rand_SOC[14];
+			Itr++;
 		}
 	}
 	return RandomValue;

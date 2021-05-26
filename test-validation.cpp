d74e421aa6429adaaad073f81f23d06f63fa8ced
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-05-26 21:32:54 +0530

    Update test-validation.cpp

--- test-validation.cpp
+++ test-validation.cpp
@@ -16,40 +16,13 @@ TEST_CASE("Test the Number printf function calls")
 	REQUIRE(printf_Func_CallCount==15);
 }
 
-
-
-TEST_CASE("Test the output Format in console") 
-{
-	ResetTestInterface();
-	
-	REQUIRE(BSM_SignalProvider(TO_CONSOLE)==0);
-	for(int Itr=0;Itr<15;Itr++)
-	{
-	REQUIRE(strcmp(ConsoleOutputFormat[Itr],"{\"Temperature\":%0.2f,\"SOC\":%0.2f}\n")==0);
-     	}
-}
-
-
-TEST_CASE("Test the output Temperature")
+TEST_CASE("Test the output Format and Temperature and SOC Values") 
 {
 	float Temp_RandNum_Min=-30.0;
 	float Temp_RandNum_Max=50.0;
-	int Rand_Temperature[15]={0,1,2,3,4,5,6,7,RAND_MAX-7,RAND_MAX-6,RAND_MAX-5,RAND_MAX-4,RAND_MAX-3,RAND_MAX-2,RAND_MAX-1};
-	
-	ResetTestInterface();
-	
-	REQUIRE(BSM_SignalProvider(TO_CONSOLE)==0);
-	for(int Itr=0;Itr<15;Itr++)
-	{
-	REQUIRE(ConsoleOutputTemperature[Itr]==(float)((((float)(Rand_Temperature[Itr])/RAND_MAX)*(Temp_RandNum_Max-Temp_RandNum_Min))+Temp_RandNum_Min));
-     	}
-}
-
-
-TEST_CASE("Test the output SOC") 
-{
 	float SOC_RandNum_Min=20.0;
 	float SOC_RandNum_Max=50.0;
+	int Rand_Temperature[15]={0,1,2,3,4,5,6,7,RAND_MAX-7,RAND_MAX-6,RAND_MAX-5,RAND_MAX-4,RAND_MAX-3,RAND_MAX-2,RAND_MAX-1};
 	int Rand_SOC[15]={0,1,2,3,4,5,6,7,RAND_MAX-7,RAND_MAX-6,RAND_MAX-5,RAND_MAX-4,RAND_MAX-3,RAND_MAX-2,RAND_MAX-1};
 	
 	ResetTestInterface();
@@ -57,6 +30,8 @@ TEST_CASE("Test the output SOC")
 	REQUIRE(BSM_SignalProvider(TO_CONSOLE)==0);
 	for(int Itr=0;Itr<15;Itr++)
 	{
+	REQUIRE(strcmp(ConsoleOutputFormat[Itr],"{\"Temperature\":%0.2f,\"SOC\":%0.2f}\n")==0);
+	REQUIRE(ConsoleOutputTemperature[Itr]==(float)((((float)(Rand_Temperature[Itr])/RAND_MAX)*(Temp_RandNum_Max-Temp_RandNum_Min))+Temp_RandNum_Min));
 	REQUIRE(ConsoleOutputSOC[Itr]==(float)((((float)(Rand_SOC[Itr])/RAND_MAX)*(SOC_RandNum_Max-SOC_RandNum_Min))+SOC_RandNum_Min));
      	}
-} 
+}

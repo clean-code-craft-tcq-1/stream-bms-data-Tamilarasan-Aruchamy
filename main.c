a0218ae33cba9ba121aa69f3024f74ea3152cce9
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-05-27 13:33:18 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
+/******************************************************* Header_Files *******************************************************/
+
 #include "SignalProvider.h"
+
+/********************************************** Function/Variable Declarations **********************************************/ 
+
 extern int BSM_SignalProvider(OutputTarget OutputTargetIndex);
+
+/*************************************************** function Definitions ***************************************************/ 
+
 int main()
 {
   BSM_SignalProvider(TO_CONSOLE);

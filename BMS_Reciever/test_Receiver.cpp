bf7dc712a69f26bb758737c0f259a4cb78a1694e
Mohammed Niyas Thottathil (RBEI/ETA22) <Thottathil.MohammedNiyas@in.bosch.com>
2021-06-21 11:33:12 +0530

    Update test_Receiver.cpp

--- BMS_Reciever/test_Receiver.cpp
+++ BMS_Reciever/test_Receiver.cpp
@@ -19,23 +19,30 @@ TEST_CASE("validate sender data")
 {
 	BMS_Receiver receiver;
 	Parameters _parameter;
-
 	
 	for (int i = 0;i < 6;i++)
 	{
 		REQUIRE(receiver.parse_data(dummy_data[i], _parameter));
-
 		auto& list = receiver.get_parameter_list();
 		list.push_back(_parameter);		
 	}
 
+	//validate list size
+	REQUIRE(receiver.get_parameter_list().size() == 6);
+
 	receiver.calculate_statistics();
 	auto stat = receiver.get_operation_instance().get_statitics();
 
+	//positive test cases - validate paramters with expected output after calculation
 	REQUIRE(compare_float(stat.min_SOC , 26.0));
 	REQUIRE(compare_float(stat.max_SOC , 66.0));
 	REQUIRE(compare_float(stat.min_Temperature , -52.0));
 	REQUIRE(compare_float(stat.max_Temperature , -24.0));
 	REQUIRE(compare_float(stat.average_SOC , 41.0));
 	REQUIRE(compare_float(stat.average_Temperature, -37.2));
+
+	//Negative test by injecting inappropriate json format
+	REQUIRE(!receiver.parse_data("aa bb cc", _parameter));
+	REQUIRE(!receiver.parse_data("{\"Temperature:-32,\"SOC\" : 36}", _parameter));
+	REQUIRE(!receiver.parse_data("{\"Temperature:one,\"SOC\" : two}", _parameter));
 }
